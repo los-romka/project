4a4529522bb82f826c566c498225f2369e1f8ca6
los-romka <sin_ss@list.ru>
2020-11-02 23:08:57 +0000

    fix

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,23 +8,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
+    private string $appEnv;
+    private bool $appDebug;
+    private string $databaseUrl;
+
+    public function __construct(string $appEnv, bool $appDebug, string $databaseUrl)
+    {
+        $this->appEnv = $appEnv;
+        $this->appDebug = $appDebug;
+        $this->databaseUrl = $databaseUrl;
+    }
+
     /**
      * @Route("/")
      */
     public function list()
     {
-        return new JsonResponse(
-            [
-                'env' => $_SERVER['APP_ENV'],
-                'list' => [
-                    random_int(0, 100),
-                    random_int(0, 100),
-                    random_int(0, 100),
-                    random_int(0, 100),
-                    random_int(0, 100),
-                    random_int(0, 100),
-                ]
-            ]
-        );
+        $data = [
+            'env' => $this->appEnv,
+            'debug' => $this->appDebug,
+            'list' => [
+                random_int(0, 100),
+                random_int(0, 100),
+                random_int(0, 100),
+                random_int(0, 100),
+                random_int(0, 100),
+                random_int(0, 100),
+            ],
+        ];
+
+        if ($this->appDebug) {
+            $data['database_url'] = $this->databaseUrl;
+        }
+
+        return new JsonResponse($data);
     }
 }
